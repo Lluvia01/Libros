769657cb915affd35af7d02eb56432a66d2d1c0a
JorgePiza <86137832+JorgePiza@users.noreply.github.com>
2021-06-19 01:57:03 -0500

    Implementación de API
    Se implementó el API phone validator, que nos permite saber si el teléfono celular existe, es solo aplicable a los números que sean del país México, así también recordarles que se debe de agregar el campo NumTelefono a la base de datos para que funcione el código, en la base de datos que ustedes contienen existe un trigger en administrador, favor de quitarlo, ya que ese hace que la contraseña sea el numero de ID del administrador

--- AgregarAdmin.php
+++ AgregarAdmin.php
@@ -1,36 +1,31 @@
 <?php
-$conexion = mysqli_connect("localhost","root","","library");
+include'conexion.php';
 
 $No_admin = $_POST['No_admin'];
 $Nombre_Admin = $_POST['Nombre_Admin'];
 $Apellido_Padmin = $_POST['Apellido_Padmin'];
 $Apellido_Madmin = $_POST['Apellido_Madmin'];
 $Correo_Admin = $_POST['Correo_Admin'];
+$NumTelefono = $_POST['NumTelefono'];
+$password = $_POST['password'];
 $data = json_decode( file_get_contents('https://api.email-validator.net/api/verify?EmailAddress='.$Correo_Admin.'&APIKey=ev-0b0756bbfb1005b30289b8fd6c8297a3'), true );
-if($data['status']==200)
+$data1 = json_decode( file_get_contents( 'https://api.phone-validator.net/api/v2/verify?PhoneNumber=+52'.$NumTelefono.'&CountryCode=mx&APIKey=pv-32d92ab9af64b0ba985bac35dea7601c'), true);
+$val1= 'MX';
+$val2= $data1['formatnational'];
+if($data['status']==200 && $data1['countrycode']== $val1)
 	{
-		$insertAdmin = "INSERT INTO `administrador`(`No_admin`, `Nombre_Admin`, `Apellido_Padmin`,`Apellido_Madmin`,`Correo_Admin`) VALUES ('$No_admin','$Nombre_Admin','$Apellido_Padmin','$Apellido_Madmin' ,'$Correo_Admin')";
+		$insertAdmin = "INSERT INTO `administrador`(`No_admin`, `Nombre_Admin`, `Apellido_Padmin`,`Apellido_Madmin`,`Correo_Admin`,`NumTelefono`) VALUES ('$No_admin','$Nombre_Admin','$Apellido_Padmin','$Apellido_Madmin' ,'$Correo_Admin', '$NumTelefono')";
  
- $No_admin = $_POST['No_admin'];
-$Nombre_Admin = $_POST['Nombre_Admin'];
-$Apellido_Padmin = $_POST['Apellido_Padmin'];
-$Apellido_Madmin = $_POST['Apellido_Madmin'];
-$Correo_Admin = $_POST['Correo_Admin'];
-
- $insertAdmin = "INSERT INTO `administrador`(`No_admin`, `Nombre_Admin`, `Apellido_Padmin`,`Apellido_Madmin`,`Correo_Admin`) VALUES ('$No_admin','$Nombre_Admin','$Apellido_Padmin','$Apellido_Madmin' ,'$Correo_Admin')";
+ 
  
  $resultado = mysqli_query($conexion,$insertAdmin);
  
 if($resultado){ 
-	$insertUsuario = "INSERT INTO `usuario`(`control`, `password_Usua`, `Nivel`) VALUES ('$No_admin',null,'1' )";
+	$insertUsuario = "INSERT INTO `usuario`(`control`, `password_Usua`, `Nivel`) VALUES ('$No_admin','$password','1' )";
 	$resultado2 = mysqli_query($conexion,$insertUsuario);
 	header('Location: Login.php');
 	    }else{
-	    	header('Location: Login.php');
-	    }
-	} else
-	{
 		echo "<script>alert('Correo invalido');</script>";
 	}
-
+}
 ?>
--- RegistroAdmin.php
+++ RegistroAdmin.php
@@ -18,10 +18,15 @@
     <label for="Apellido_Padmin">Apellido Paterno</label>
     <input type="text" name="Apellido_Padmin"  placeholder="Ingrese su Apellido">
     <label for="Apellido_Madmin">Apellido Materno</label>
-    <input type="text" name="Apellido_Madmin"  placeholder="Ingrese su Apellido">
+    <input type="text" name="Apellido_Madmin"  placeholder="Ingrese su Apellido"><br>
     <label for="email">Correo</label>
     <input type="email" name="Correo_Admin"  placeholder="Ingrese su Correo">
 
+    <label for="NumTelefono">Numero telefonico</label>
+    <input type="text" name="NumTelefono"  placeholder="Ingrese su Numero telefonico">
+    <label for="password">Contraseña</label>
+    <input type="password" name="password"  required placeholder="Ingresa tu contrase&ntilde;a"><br><br>
+<br>
     <input type="submit" value="Registrar">
     <p><a href="Login.php">Ya tengo Cuenta</a></p>
   </form>
